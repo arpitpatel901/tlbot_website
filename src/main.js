f08c185df9ace31f9b6320000ca66f7360accc7a
Margi0411 <166356886+Margi0411@users.noreply.github.com>
2024-04-11 19:45:58 +0530

    Google cloud project done Login button working till getting profile info code (#Buggy code)

--- src/main.js
+++ src/main.js
@@ -4,15 +4,19 @@ import { createApp } from 'vue'
 import PrimeVue from 'primevue/config';
 import Lara from '@/presets/lara';      //import preset  
 // import Wind from '@/presets/wind';      //import preset        
-
+import vue3GoogleLogin from "vue3-google-login";
 
 import App from './App.vue';
 
+export const CLIENT_ID = "497764252617-oau8a78f5pcolh165ntbm36e9f3d3hgh.apps.googleusercontent.com";
 const app = createApp(App);
 app.use(PrimeVue, {
     unstyled: true,
     pt: Lara                            //apply preset 
     // pt: Wind                            //apply preset      
+},
+vue3GoogleLogin, {
+    clientId: CLIENT_ID
 });
 
 app.mount('#app');
